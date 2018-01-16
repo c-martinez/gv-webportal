47c743fdb9ab86aae0d6e0ab80a48c59a29ae74f
Carlos <c.martinez@esciencecenter.nl>
2018-01-16 16:49:12 +0100

    Add baselayer

---
+++ src/app/services/baselayer.ts
@@ -0,0 +1,8 @@
+export class BaseLayer {
+    public id: number;
+    public name: string;
+    public active: boolean;
+
+    constructor() {}
+
+}
--- src/app/services/layer.service.ts
+++ src/app/services/layer.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 
+import { BaseLayer } from './baselayer';
+
 @Injectable()
 export class LayerService {
-  private layers = [];
+  private layers: BaseLayer[] = [];
 
   constructor() {
-    this.addLayer({ name: 'Layer 1', id: 21, active: true });
+    this.addLayer({ name: 'Layer 1', id: 21, active: true  });
     this.addLayer({ name: 'Layer 2', id: 24, active: false });
   }
 
-  public addLayer(layer: any) {
+  public addLayer(layer: BaseLayer) {
     this.layers.push(layer);
   }
 
